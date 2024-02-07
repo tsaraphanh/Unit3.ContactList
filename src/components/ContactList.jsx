f91a43eb65ee28467e37010b27ffa3ad9f60915d
Troy Saraphanh <troysaraphanh@Troys-MacBook-Air.local>
2024-02-06 18:47:35 -0600

    added useEffect hook to api fetch in ContactList.jsx

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import ContactRow from "./ContactRow";
+import { useEffect } from "react";
 
 const dummyContacts = [
   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -11,7 +12,19 @@ const dummyContacts = [
 export default function ContactList() {
 
   const [contacts, setContacts] = useState(dummyContacts);
-    console.log("Contacts: ", contacts);
+  useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts()
+  }, [])
+  console.log("Contacts: ", contacts);
     return (
         <table>
           <thead>
